1e32a74b6bfb90f035fafa719fc84f5444f3cd05
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-10-19 16:45:35 +0200

    docs

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {
   AutomergeModel: require('./lib/AutomergeModel'),
   AutomergeHttp: require('./lib/AutomergeHttp'),
   AutomergeMiddleware: require('./lib/AutomergeMiddleware'),
+  AutomergeTestHelper: require('./lib/AutomergeTestHelper'),
 }
